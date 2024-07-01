3f2be89addc4d8eabe7cee8135bb2d12e26ce5dd
AlexC1991 <114543336+AlexC1991@users.noreply.github.com>
2024-07-01 22:45:32 +1000

    CStr(), ToUpper(), ToLower(), Find(), Replace().
    Function CStrl() Added, Function ToUpper() Added, Function ToLower() Added, Function Find() Added, Function Replace() Added.
    
    Cleaned up execution layout of test code.
    
    Changed CharacterAt() function to work as required and not like a find function.

--- String.cpp
+++ String.cpp
@@ -29,9 +29,6 @@ size_t String::Length() const
 
 char & String::CharacterAt(size_t _index)
 {
-   if (_index >= length) {
-      throw std::out_of_range("Index out of range");
-   }
    return charDataVariab[_index];
 }
 
@@ -96,9 +93,11 @@ String & String::ToUpper()
 size_t String::Find(const String &_str)
 {
    const char* found = std::strstr(charDataVariab, _str.charDataVariab);
-   if (found) {
+   if (found)
+   {
       return found - charDataVariab;
    }
+
    return std::string::npos;
 }
 
@@ -114,10 +113,39 @@ size_t String::Find(size_t _startIndex, const String &_str)
    return std::string::npos;
 }
 
-String & String::Replace(const String &_find, const String &_replace) {
+String & String::Replace(const String &_find, const String &_replace)
+{
+   size_t pos = Find(_find);
+   while (pos != static_cast<size_t>(-1))
+   {
+
+      size_t newLength = length - _find.length + _replace.length;
+      char* newCharData = new char[newLength + 1];
+
+      std::strncpy(newCharData, charDataVariab, pos);
+      std::strcpy(newCharData + pos, _replace.CStr());
+      std::strcpy(newCharData + pos + _replace.length, charDataVariab + pos + _find.length);
+
+      delete[] charDataVariab;
+      charDataVariab = newCharData;
+      length = newLength;
+
+      pos = Find(pos + _replace.length, _find);
+   }
+   return *this;
 }
 
-String & String::ReadFromConsole() {
+String & String::ReadFromConsole()
+{
+   char buffer[1024];
+   std::cin.getline(buffer, 1024);
+
+   delete[] charDataVariab;
+   length = std::strlen(buffer);
+   charDataVariab = new char[length + 1];
+   std::strcpy(charDataVariab, buffer);
+
+   return *this;
 }
 
 String & String::WriteToConsole()
@@ -144,11 +172,16 @@ const char & String::operator[](size_t _index) const {
 int main()
 {
    // Start of script.
+   std::cout << "-----------" << std::endl;
+   std::cout << "Start of Script" << std::endl;
+   std::cout << "-----------" << std::endl;
    String myStringClass("Alex");
    myStringClass.WriteToConsole();
    std::cout << "----------------------" << std::endl;
 
    // Checking Length of Char.
+   std::cout << "Test One" << std::endl;
+   std::cout << "Char Length Test Start" << std::endl;
    if (myStringClass.Length() > 0)
    {
       std::cout << "Char Length: " << myStringClass.Length() << std::endl;
@@ -157,8 +190,11 @@ int main()
    }
 
    // CharacterAt Lookup Method Of Char "Alex"
-   char *letterToFind = "x";
-   int indexToLookup = 3;
+   int indexToLookup = 0;
+   char thisLetterFoundInstead = myStringClass.CharacterAt(indexToLookup);
+
+   std::cout << "Test Two" << std::endl;
+   std::cout << "CharacterAt Test Start" << std::endl;
 
    // First checks if index is out of range
    if (indexToLookup >= myStringClass.Length())
@@ -168,47 +204,42 @@ int main()
       std::cout << "----------------------" << std::endl;
    }
    else
-      // Second step checks if the character at the given index matches the letterToFind
-      if (myStringClass.CharacterAt(indexToLookup) != *letterToFind)
-      {
-         std::cout << "Unable to find " << letterToFind << " at index " << indexToLookup << " of Char " << std::endl;
-         char thisLetterFoundInstead = myStringClass.CharacterAt(indexToLookup);
-         std::cout << "Found " << thisLetterFoundInstead << " at index " << indexToLookup << " of Char " << std::endl;
-         std::cout << "----------------------" << std::endl;
-      }
-      else
-      {
-         std::cout << "Found " << letterToFind << " at index " << indexToLookup << " of Char " << std::endl;
-         std::cout << "CharacterAt Test Complete" << std::endl;
-         std::cout << "----------------------" << std::endl;
-      }
-   // removes char from memory.
-   --letterToFind;
+   {
+      // Second checks what char character is at index.
+      std::cout << "Found " << thisLetterFoundInstead << " at index " << indexToLookup << " of Char " << std::endl;
+      std::cout << "CharacterAt Test Complete" << std::endl;
+      std::cout << "----------------------" << std::endl;
+   }
 
    // Append Value Added (At End Of Char)
+   std::cout << "Test Three" << std::endl;
+   std::cout << "Append Test Start" << std::endl;
    myStringClass.Append("h");
    myStringClass.WriteToConsole();
    std::cout << "Append Test Complete" << std::endl;
    std::cout << "----------------------" << std::endl;
 
    // Prepend Value Added (At Start Of Char)
+   std::cout << "Test Four" << std::endl;
+   std::cout << "Prepend Test Start" << std::endl;
    myStringClass.Prepend("h");
    myStringClass.WriteToConsole();
    std::cout << "Prepend Test Complete" << std::endl;
    std::cout << "----------------------" << std::endl;
 
    // Checking if Values Are Equal Char
-
    char *equalCharValue ="hAlexh";
+   std::cout << "Test Five" << std::endl;
+   std::cout << "EqualTo Test Start" << std::endl;
    if (myStringClass.EqualTo(equalCharValue))
    {
-      std::cout << equalCharValue << " is equal to the base string char" << std::endl;
+      std::cout << equalCharValue << " is equal to " << myStringClass.CStr() << std::endl;
       std::cout << "EqualTo Test Complete" << std::endl;
       std::cout << "----------------------" << std::endl;
    }
    else
    {
-      std::cout << equalCharValue << " is not equal to the base string char" << std::endl;
+      std::cout << equalCharValue << " is not equal to " << myStringClass.CStr() << std::endl;
       std::cout << "EqualTo Test Failed" << std::endl;
       std::cout << "----------------------" << std::endl;
    }
@@ -216,6 +247,65 @@ int main()
    // removes char from memory.
  --equalCharValue;
 
-   // Starts again from top to bottom.
+   // Returns CStr and displays it.
+   std::cout << "Test Six" << std::endl;
+   std::cout << "CStr Test Start" << std::endl;
+   std::cout << "Using CStr() Test:  " << myStringClass.CStr() << std::endl;
+   std::cout << "CStr Test Complete" << std::endl;
+   std::cout << "----------------------" << std::endl;
+
+   // Displays the char in all uppercase.
+   std::cout << "Test Seven" << std::endl;
+   std::cout << "ToUpper Test Start" << std::endl;
+   myStringClass.ToUpper();
+   myStringClass.WriteToConsole();
+   std::cout << "ToUpper Test Complete" << std::endl;
+   std::cout << "----------------------" << std::endl;
+
+   // Displays the char in all lowercase.
+   std::cout << "Test Eight" << std::endl;
+   std::cout << "ToLower Test Start" << std::endl;
+   myStringClass.ToLower();
+   myStringClass.WriteToConsole();
+   std::cout << "ToLower Test Complete" << std::endl;
+   std::cout << "----------------------" << std::endl;
+
+   // Finds location of the char character.
+   std::cout << "Test Nine" << std::endl;
+   std::cout << "Find Test Start" << std::endl;
+   char *findThis= "l";
+   size_t locationFoundIn = myStringClass.Find(findThis);
+   // First checks if letter to find is out of range
+   if (locationFoundIn > myStringClass.Length() || locationFoundIn <= 0 )
+   {
+      std::cout << findThis << " <- This is not in " << myStringClass.CStr() << std::endl;
+      std::cout << "Find Test Failed" << std::endl;
+      std::cout << "----------------------" << std::endl;
+   }
+   else
+         {
+            std::cout << "Found " << findThis << " at index " << locationFoundIn << " of Char " << std::endl;
+            std::cout << "Find Test Complete" << std::endl;
+            std::cout << "----------------------" << std::endl;
+         }
+
+   // removes char from memory.
+   --findThis;
+
+   // Replaces Char with Another
+   std::cout << "Test Ten" << std::endl;
+   std::cout << "Replace Test Start" << std::endl;
+   char *replacementChar = "Alex";
+   myStringClass.Replace(myStringClass.CStr(), replacementChar);
+   myStringClass.WriteToConsole();
+   std::cout << "Replace Test Complete" << std::endl;
+   std::cout << "----------------------" << std::endl;
+
+   // removes char from memory.
+   --replacementChar;
+
+
+
+   // Starts again from line 0 in the .cpp script.
    return 0;
 }
