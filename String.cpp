9029cc9e8a8afc77ecede3cd763ca2e74e1118d9
AlexC1991 <114543336+AlexC1991@users.noreply.github.com>
2024-07-01 21:14:57 +1000

    Append(), Prepend(), EqualTo() Functions Added
    Added Append Function, Added Prepend() Function, Added EqualTo() Functio.
    
    Cleaned up CharacterAt Method & WriteToConsole Function.

--- String.cpp
+++ String.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 #include "String.h"
 #include <cstring>
-
 #include <string.h>
-
-String::String() : charDataVariab(nullptr), length(0)
-{
-   std::cout << "String Name: " << (charDataVariab ? charDataVariab : "null") << std::endl;
-}
 
 String::String(const char *_str)
 {
@@ -16,8 +10,11 @@ String::String(const char *_str)
    std::strcpy(charDataVariab, _str);
 }
 
-String::String(const String &_other) {
-
+String::String(const String &_other)
+{
+   length = _other.length;
+   charDataVariab = new char[length + 1];
+   std::strcpy(charDataVariab, _other.charDataVariab);
 }
 
 String::~String()
@@ -38,31 +35,83 @@ char & String::CharacterAt(size_t _index)
    return charDataVariab[_index];
 }
 
-const char & String::CharacterAt(size_t _index) const {
+const char & String::CharacterAt(size_t _index) const
+{
+   if (_index >= length) {
+      throw std::out_of_range("Index out of range");
+   }
+   return charDataVariab[_index];
 }
 
-bool String::EqualTo(const String &_other) const {
+bool String::EqualTo(const String &_other) const
+{
+   return std::strcmp(charDataVariab, _other.charDataVariab) == 0;
 }
 
-String & String::Append(const String &_str) {
+String & String::Append(const String &_str)
+{
+   size_t newLength = length + _str.length;
+   char* newCharDataVariab = new char[newLength + 1];
+   std::strcpy(newCharDataVariab, charDataVariab);
+   std::strcat(newCharDataVariab, _str.charDataVariab);
+   delete[] charDataVariab;
+   charDataVariab = newCharDataVariab;
+   length = newLength;
+   return *this;
 }
 
-String & String::Prepend(const String &_str) {
+String & String::Prepend(const String &_str)
+{
+   size_t newLength = length + _str.length;
+   char* newCharDataVariab = new char[newLength + 1];
+   std::strcpy(newCharDataVariab, _str.charDataVariab);
+   std::strcat(newCharDataVariab, charDataVariab);
+   delete[] charDataVariab;
+   charDataVariab = newCharDataVariab;
+   length = newLength;
+   return *this;
 }
 
-const char * String::CStr() const {
+const char * String::CStr() const
+{
+   return charDataVariab;
 }
 
-String & String::ToLower() {
+String & String::ToLower()
+{
+   for (size_t i = 0; i < length; ++i) {
+      charDataVariab[i] = std::tolower(charDataVariab[i]);
+   }
+   return *this;
 }
 
-String & String::ToUpper() {
+String & String::ToUpper()
+{
+   for (size_t i = 0; i < length; ++i) {
+      charDataVariab[i] = std::toupper(charDataVariab[i]);
+   }
+   return *this;
 }
 
-size_t String::Find(const String &_str) {
+size_t String::Find(const String &_str)
+{
+   const char* found = std::strstr(charDataVariab, _str.charDataVariab);
+   if (found) {
+      return found - charDataVariab;
+   }
+   return std::string::npos;
 }
 
-size_t String::Find(size_t _startIndex, const String &_str) {
+size_t String::Find(size_t _startIndex, const String &_str)
+{
+   if (_startIndex >= length) {
+      return std::string::npos;
+   }
+   const char* found = std::strstr(charDataVariab + _startIndex, _str.charDataVariab);
+   if (found) {
+      return found - charDataVariab;
+   }
+   return std::string::npos;
 }
 
 String & String::Replace(const String &_find, const String &_replace) {
@@ -73,7 +122,7 @@ String & String::ReadFromConsole() {
 
 String & String::WriteToConsole()
 {
-   std::cout << charDataVariab << std::endl;
+   std::cout << "String Name: " << (charDataVariab ? charDataVariab : "null") << std::endl;
    return *this;
 }
 
@@ -94,18 +143,79 @@ const char & String::operator[](size_t _index) const {
 
 int main()
 {
+   // Start of script.
    String myStringClass("Alex");
    myStringClass.WriteToConsole();
+   std::cout << "----------------------" << std::endl;
 
-   std::cout << "Char Length: " << myStringClass.Length() << std::endl;
-   std::cout << "Char Length Test Complete" << std::endl;
-
-   try {
-      char& ch = myStringClass.CharacterAt(1);
-      std::cout << "Character at index 1: " << ch << std::endl;
-   } catch (const std::out_of_range& e) {
-      std::cerr << e.what() << std::endl;
+   // Checking Length of Char.
+   if (myStringClass.Length() > 0)
+   {
+      std::cout << "Char Length: " << myStringClass.Length() << std::endl;
+      std::cout << "Char Length Test Complete" << std::endl;
+      std::cout << "----------------------" << std::endl;
    }
 
+   // CharacterAt Lookup Method Of Char "Alex"
+   char *letterToFind = "x";
+   int indexToLookup = 3;
+
+   // First checks if index is out of range
+   if (indexToLookup >= myStringClass.Length())
+   {
+      std::cout << indexToLookup << " <- This is out of Range" << std::endl;
+      std::cout << "CharacterAt Test Failed" << std::endl;
+      std::cout << "----------------------" << std::endl;
+   }
+   else
+      // Second step checks if the character at the given index matches the letterToFind
+      if (myStringClass.CharacterAt(indexToLookup) != *letterToFind)
+      {
+         std::cout << "Unable to find " << letterToFind << " at index " << indexToLookup << " of Char " << std::endl;
+         char thisLetterFoundInstead = myStringClass.CharacterAt(indexToLookup);
+         std::cout << "Found " << thisLetterFoundInstead << " at index " << indexToLookup << " of Char " << std::endl;
+         std::cout << "----------------------" << std::endl;
+      }
+      else
+      {
+         std::cout << "Found " << letterToFind << " at index " << indexToLookup << " of Char " << std::endl;
+         std::cout << "CharacterAt Test Complete" << std::endl;
+         std::cout << "----------------------" << std::endl;
+      }
+   // removes char from memory.
+   --letterToFind;
+
+   // Append Value Added (At End Of Char)
+   myStringClass.Append("h");
+   myStringClass.WriteToConsole();
+   std::cout << "Append Test Complete" << std::endl;
+   std::cout << "----------------------" << std::endl;
+
+   // Prepend Value Added (At Start Of Char)
+   myStringClass.Prepend("h");
+   myStringClass.WriteToConsole();
+   std::cout << "Prepend Test Complete" << std::endl;
+   std::cout << "----------------------" << std::endl;
+
+   // Checking if Values Are Equal Char
+
+   char *equalCharValue ="hAlexh";
+   if (myStringClass.EqualTo(equalCharValue))
+   {
+      std::cout << equalCharValue << " is equal to the base string char" << std::endl;
+      std::cout << "EqualTo Test Complete" << std::endl;
+      std::cout << "----------------------" << std::endl;
+   }
+   else
+   {
+      std::cout << equalCharValue << " is not equal to the base string char" << std::endl;
+      std::cout << "EqualTo Test Failed" << std::endl;
+      std::cout << "----------------------" << std::endl;
+   }
+
+   // removes char from memory.
+ --equalCharValue;
+
+   // Starts again from top to bottom.
    return 0;
 }
